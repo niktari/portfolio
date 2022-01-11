9298b1e3f48713cbbf86dc801b84d0e6b609cfdd
Nikki Makagiansar <nmakagiansar@gmail.com>
2022-01-11 13:37:32 -0500

    fix scroll

--- main.js
+++ main.js
@@ -27,15 +27,39 @@ window.onscroll = function () {
     }
 }
 
-top_button.onclick = function () {
+// top_button.onclick = function () {
 
 
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+//     window.scrollTo({ top: 0, behavior: 'smooth' });
 
-    // document.body.scrollTop = 0;
-    // document.documentElement.scrollTop = 0;
+//     // document.body.scrollTop = 0;
+//     // document.documentElement.scrollTop = 0;
 
-}
+// }
+
+// Add smooth scrolling to all links
+$("#top-button-wrapper").on('click', function (event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function () {
+
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+
+        });
+    } // End if
+});
 
 function show_info() {
     info_overlay.style.display = 'block';
