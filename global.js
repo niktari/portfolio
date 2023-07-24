82da37c5ae378563a900c8908b50d4331257856e
niktari <77365760+niktari@users.noreply.github.com>
2023-07-24 17:52:56 -0400

    fix script

--- global.js
+++ global.js
@@ -12,27 +12,27 @@ let i = document.getElementById('info-container');
 
 let c = document.getElementById('close');
 
-i_link.onclick = function show_info(event){
-    i.style.display = 'block'
-    event.preventDefault();
-}
+// i_link.onclick = function show_info(event){
+//     i.style.display = 'block'
+//     event.preventDefault();
+// }
 
-c.onclick = function hide_info(){
-    i.style.display = 'none';
-}
+// c.onclick = function hide_info(){
+//     i.style.display = 'none';
+// }
 
-let h_c = document.getElementById('header-container')
-let m_n = document.getElementById('my-name')
+// let h_c = document.getElementById('header-container')
+// let m_n = document.getElementById('my-name')
 
-document.onscroll = function() {
+// document.onscroll = function() {
 
-    if(scrollY === 0){
-        h_c.style.backgroundColor = 'transparent';
-        m_n.style.opacity = 0;
-    } else {
-        h_c.style.backgroundColor = getComputedStyle(document.body).backgroundColor;
-        m_n.style.opacity = 1;
-    }
+//     if(scrollY === 0){
+//         h_c.style.backgroundColor = 'transparent';
+//         m_n.style.opacity = 0;
+//     } else {
+//         h_c.style.backgroundColor = getComputedStyle(document.body).backgroundColor;
+//         m_n.style.opacity = 1;
+//     }
     
-}
+// }
 
